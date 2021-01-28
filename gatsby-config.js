32c0e3542ae89825e5713482e4a96784635de7b6
pixendrit <65007484+pixendrit@users.noreply.github.com>
2021-01-28 12:39:48 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // Of course you can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitleAlt: `differente`,
   },
   plugins: [
     {
@@ -28,7 +28,7 @@ module.exports = {
       options: {
         name: `Cara - @lekoarts/gatsby-theme-cara`,
         short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        description: `different`,
         start_url: `/`,
         background_color: `#141821`,
         theme_color: `#f6ad55`,
